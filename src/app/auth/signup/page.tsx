e98d3cb8fdbcfc0f25677f4836ca3cd9d6680fc6
luiz inaldo <luiz.inaldo970@gmail.com>
2024-05-27 13:48:28 -0300

    feat: adicionado o componente de botão na tela de cadastro

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -402,7 +402,7 @@ const SignUp: React.FC = () => {
                     value="Criar uma conta"
                     className="w-full cursor-pointer rounded-lg border border-primary bg-primary p-4 text-white transition hover:bg-opacity-90"
                   /> */}
-                  <SubmitButton />
+                  <SubmitButton value={'Criar uma conta'}/>
                 </div>
 
                 <button disabled className="flex w-full items-center justify-center gap-3.5 rounded-lg border border-stroke bg-gray p-4 hover:bg-opacity-50 dark:border-strokedark dark:bg-meta-4 dark:hover:bg-opacity-50 disabled:opacity-50 cursor-not-allowed">
