1af15e4319befa6694e51ea47e159bd5fa1b6dc2
Jarbas Gouveia <gouvik.dev@gmail.com>
2024-05-28 15:35:08 -0300

    feat: Atualizar configuração de imagens para adicionar domínio de desenvolvimento

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        domains: ['127.0.0.1', 'ativos-cvld-prod-32c6589080c0.herokuapp.com', 'ativoscvld.vercel.app'],
         remotePatterns: [
             {
+                protocol: 'https',
                 hostname: 'ativos-cvld-prod-32c6589080c0.herokuapp.com',
                 port: '',
                 pathname: '/**'
             },
-            // {
-            //     protocol: 'http',
-            //     hostname: 'ativos-cvld-prod-32c6589080c0.herokuapp.com',
-            //     port: '',
-            //     pathname: '/**'
-            // },
             {
                 protocol: 'http',
-                hostname: '127.0.0.1:3000',
+                hostname: '127.0.0.1:8000',
                 port: '',
                 pathname: '/**'
             },
             {
                 protocol: 'https',
                 hostname: 'ativoscvld.vercel.app',
-                port: '',
-                pathname: '/**'
-            },
-            {
-                protocol: 'https',
-                hostname: 'dev-ativoscvld.vercel.app',
                 port: '',
                 pathname: '/**'
             }
