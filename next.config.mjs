8af3ec3032c26d6b05c3f23e1b4e3cd0ed354709
Jarbas Gouveia <gouvik.dev@gmail.com>
2024-05-28 12:46:34 -0300

    feat: Atualizar configuração de imagens para adicionar domínio de desenvolvimento

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,16 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: 'https',
                 hostname: 'ativos-cvld-prod-32c6589080c0.herokuapp.com',
                 port: '',
                 pathname: '/**'
             },
+            // {
+            //     protocol: 'http',
+            //     hostname: 'ativos-cvld-prod-32c6589080c0.herokuapp.com',
+            //     port: '',
+            //     pathname: '/**'
+            // },
             {
                 protocol: 'http',
                 hostname: '127.0.0.1:3000',
