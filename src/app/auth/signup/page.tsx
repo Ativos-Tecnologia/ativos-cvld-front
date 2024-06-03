babfe875c0005f42bff84a3b7d400c5cbb953387
Jarbas Gouveia <gouvik.dev@gmail.com>
2024-06-03 11:39:36 -0300

    refactor: Corrigir erro de upload

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -329,13 +329,11 @@ const SignUp: React.FC = () => {
                       })
                       }
                     />
-
                     <ErrorMessage errors={errors} field='password' />
-
                     <span className="absolute right-4 top-4">
                       <BiLockAlt style={{ width: '22px', height: '22px', fill: '#BAC1CB' }} />
                     </span>
-                  </div>j
+                  </div>
                 </div>
 
                 <div className="mb-12">
