a5c1918370d9c99b129d9812786019bdd29b20b8
Jarbas Gouveia <gouvik.dev@gmail.com>
2024-06-04 12:21:06 -0300

    feat: Add subscription data to UserInfoContext
    This commit adds the `subscriptionData` property to the `UserInfoContextType` interface and the `UserInfoAPIContext` context. It also initializes the `subscriptionData` state in the `UserInfoProvider` component and fetches the subscription data from the server.

--- src/context/UserInfoContext.tsx
+++ src/context/UserInfoContext.tsx
@@ -21,6 +21,7 @@ export interface UserInfoContextType {
     updateProfile: (id: string, data: any) => Promise<any>;
     firstLogin: boolean | null;
     setFirstLogin: (value: boolean | null) => void;
+    subscriptionData: ISubscriptionInfo | null;
 }
 
 
@@ -31,6 +32,20 @@ export interface UpdateUserProfile extends FormData {
     phone: string;
     profile_picture: string;
     title: string;
+}
+
+export type SubscriptionStatus = "PENDING" | "ACTIVE" | "CANCELLED" | "EXPIRED"
+
+export type SubscriptionPlan = "FREE" | "BASIC" | "PREMIUM" | "ENTERPRISE"
+
+export interface ISubscriptionInfo {
+    id: string;
+    user: number;
+    status: SubscriptionStatus;
+    plan: SubscriptionPlan;
+    start_date: string;
+    end_date: string;
+
 }
 
 export const UserInfoAPIContext = createContext<UserInfoContextType>({
@@ -48,13 +63,15 @@ export const UserInfoAPIContext = createContext<UserInfoContextType>({
     error: null,
     updateProfile: async () => ({}),
     firstLogin: null,
-    setFirstLogin: () => {}
+    setFirstLogin: () => {},
+    subscriptionData: null,
 })
 
 
 export const UserInfoProvider = ({ children }: { children: React.ReactNode }) => {
 
     const [firstLogin, setFirstLogin] = useState<boolean | null>(null);
+    const [subscriptionData, setSubscriptionData] = useState<ISubscriptionInfo | null>(null);
 
     const [data, setData] = useState<UserInfo>({
         0: {
@@ -70,24 +87,60 @@ export const UserInfoProvider = ({ children }: { children: React.ReactNode }) =>
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
 
-    useEffect(() => {
-        const fetchProfile = async () => {
-            try {
-                const response = await api.get("/api/profile/", {
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                });
+    // const checkUserSubscription = async () => {
+    //     try {
+    //         const response = await api.get("/api/subscription/", {
+    //             headers: {
+    //                 "Content-Type": "application/json",
+    //             },
+    //         });
 
-                setData(response.data);
-                setLoading(false);
-            } catch (error: any) {
-                setError(error.message);
-                setLoading(false);
-                console.error(error);
+    //         return response.data;
+    //     } catch (error: any) {
+    //         console.error(error);
+    //         return error
+    //     }
+    // }
+
+    // const fetchProfile = async () => {
+    //     try {
+    //         const response = await api.get("/api/profile/", {
+    //             headers: {
+    //                 "Content-Type": "application/json",
+    //             },
+    //         });
+
+    //         setData(response.data);
+    //         // setLoading(false);
+    //     } catch (error: any) {
+    //         setError(error.message);
+    //         setLoading(false);
+    //         console.error(error);
+    //     }
+    // }
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            const [profileResult, subscriptionResult] = await Promise.all([api.get("/api/profile/"), api.get("/api/user/get-subscription-info/")]);
+
+            if (profileResult.status === 200) {
+                setData(profileResult.data);
+            } else {
+                setError("Error fetching profile data");
             }
-        }
-        fetchProfile();
+
+            if (subscriptionResult.status === 200) {
+                setSubscriptionData(subscriptionResult.data);
+            } else {
+                setError("Error fetching subscription data");
+            }
+
+            setLoading(false);
+        };
+
+        fetchData();
     }, []);
 
     const updateProfile = async (id: string, data: UpdateUserProfile) => {
@@ -116,10 +169,8 @@ export const UserInfoProvider = ({ children }: { children: React.ReactNode }) =>
         }
     }
 
-
-
     return (
-        <UserInfoAPIContext.Provider value={{ data, loading, error, updateProfile, firstLogin, setFirstLogin }}>
+        <UserInfoAPIContext.Provider value={{ data, loading, error, updateProfile, firstLogin, setFirstLogin, subscriptionData }}>
             {children}
         </UserInfoAPIContext.Provider>
     );
