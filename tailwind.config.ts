6fb9a7d6a9e3c18c3931d1d59e068e6798e04cb6
Jarbas Gouveia <gouvik.dev@gmail.com>
2024-06-17 13:18:09 -0300

    feat: Add prussianBlue color to tailwind.config.ts
    This commit adds the prussianBlue color to the tailwind.config.ts file. The prussianBlue color is used to enhance the visual design of the application by providing a new color option for various UI elements.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,6 +59,7 @@ const config: Config = {
         success: "#219653",
         danger: "#D34053",
         warning: "#FFA70B",
+        prussianBlue: "#003153",
       },
       fontSize: {
         "title-xxl": ["44px", "55px"],
