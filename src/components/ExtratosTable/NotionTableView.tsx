24ec83d45eb6390ec97a21a93481b6b72098edb5
Jarbas Gouveia <gouvik.dev@gmail.com>
2024-08-29 22:59:45 -0300

    fix: reativa i interval

--- src/components/ExtratosTable/NotionTableView.tsx
+++ src/components/ExtratosTable/NotionTableView.tsx
@@ -262,7 +262,7 @@ const NotionTableView = ({ count, setExtratosTableToNotionDrawersetId, setNotion
             queryKey: ['notion_list'],
             refetchOnReconnect: true,
             refetchOnWindowFocus: true,
-            // refetchInterval: 1000 * 15, // 15 seconds
+            refetchInterval: 1000 * 15, // 15 seconds
             staleTime: 1000 * 13, // 13 seconds
             queryFn: fetchNotionData,
             enabled: !!data2?.user // only fetch if user is defined after context is loaded
