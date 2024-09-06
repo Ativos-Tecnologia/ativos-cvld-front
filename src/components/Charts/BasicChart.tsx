81c5217da1ccbc8102c43dc7a2767c06dd4ab92e
Jarbas Gouveia <gouvik.dev@gmail.com>
2024-09-06 14:58:29 -0300

    refact: remove unused chart components and update BasicChart layout

--- src/components/Charts/BasicChart.tsx
+++ src/components/Charts/BasicChart.tsx
@@ -1,8 +1,6 @@
 "use client";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
-import RentabilityChart from "@/components/Charts/RentabilityChart";
-import ProfitChart from "@/components/Charts/ProfitBarChart";
-import DistributionChart from "@/components/Charts/DistributionChart";
+
 
 const BasicChart: React.FC = () => {
   return (
@@ -10,9 +8,7 @@ const BasicChart: React.FC = () => {
       <Breadcrumb pageName="Basic Chart" />
 
       <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
-        <RentabilityChart />
-        <ProfitChart />
-        <DistributionChart title="Chart Three" />
+
       </div>
     </>
   );
