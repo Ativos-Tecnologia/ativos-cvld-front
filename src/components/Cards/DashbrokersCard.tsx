1b17140ee112bddf449884c68d56bc4cb3b5626f
Luiz Inaldo <luiz.inaldo970@gmail.com>
2024-10-29 11:39:36 -0300

    feat: Atualização nos cards da view brokers
    ### Componentes que sofreram alteração nesse commit:
    
    > auth/signup:
    - Mudança básica de estilização e texto no que diz respeito ao aviso dos dados bancários
    
    > Settings:
    - Momentaneamente, esse componente recebeu o hook useQuery, para realizar requisições (a fim de resgatar os dados dos cards)
    
    > DashbrokersCard:
    - Foi retirado todo tipo de dado mockado antes usado para testes dos cards.
    
    > NewForm:
    - Mudanças no toast de sucesso/erro ao salvar os dados de proposta e comissão.
    - Adicionado toast de sucesso/erro ao salvar os dados do ofício no notion.
    - Removidos imports que não eram mais usados.

--- src/components/Cards/DashbrokersCard.tsx
+++ src/components/Cards/DashbrokersCard.tsx
@@ -1,24 +1,29 @@
 import numberFormat from '@/functions/formaters/numberFormat'
 import React, { useEffect, useState } from 'react'
 import CustomCheckbox from '../CrmUi/Checkbox';
-import { BiPlus } from 'react-icons/bi';
-import { BsCheckCircleFill, BsFillPatchCheckFill } from 'react-icons/bs';
+import { BiCheck, BiSave, BiX } from 'react-icons/bi';
+import { BsCheckCircleFill } from 'react-icons/bs';
 import { FaRegFilePdf } from 'react-icons/fa';
+import { Button } from '../Button';
+import { NotionPage } from '@/interfaces/INotion';
+import api from '@/utils/api';
+import { toast } from 'sonner';
 
-const DashbrokersCard = () => {
+const DashbrokersCard = ({ oficio }: { oficio: NotionPage }) => {
 
     /* ====> value states <==== */
-    const [proposalValue, setProposalValue] = useState({ min: 15000, max: 30000 });
-    const [comissionValue, setComissionValue] = useState({ min: 7500, max: 9500 });
     const [sliderValues, setSliderValues] = useState({
         proposal: 0,
         comission: 0,
     });
+    const [savingProposalAndComission, setSavingProposalAndComission] = useState<boolean>(false);
 
     // Função para atualizar a proposta e ajustar a comissão proporcionalmente
     const handleProposalSliderChange = (
         e: React.ChangeEvent<HTMLInputElement>,
     ) => {
+
+        // seta o valor do slide como o valor atual
         const newProposalSliderValue = parseFloat(e.target.value);
         setSliderValues((oldValues) => {
             return { ...oldValues, proposal: newProposalSliderValue };
@@ -26,11 +31,14 @@ const DashbrokersCard = () => {
 
         // Calcular a proporção em relação a proposta e ajustar a comissão
         const proportion =
-            (newProposalSliderValue - proposalValue.min) /
-            (proposalValue.max - proposalValue.min);
+            (newProposalSliderValue - (oficio.properties["(R$) Proposta Mínima - Celer"].formula?.number || 0)) /
+            ((oficio.properties["(R$) Proposta Máxima - Celer"].formula?.number || 0) - (oficio.properties["(R$) Proposta Mínima - Celer"].formula?.number || 0));
+
+        // define o novo valor da comissão em relação a proporção
         const newComissionSliderValue =
-            comissionValue.max -
-            proportion * (comissionValue.max - comissionValue.min);
+            (oficio.properties["(R$) Comissão Máxima - Celer"].number || 0) -
+            proportion * ((oficio.properties["(R$) Comissão Máxima - Celer"].number || 0) - (oficio.properties["(R$) Comissão Mínima - Celer"].number || 0));
+
         setSliderValues((oldValues) => {
             return { ...oldValues, comission: newComissionSliderValue };
         });
@@ -40,6 +48,8 @@ const DashbrokersCard = () => {
     const handleComissionSliderChange = (
         e: React.ChangeEvent<HTMLInputElement>,
     ) => {
+
+        // seta o valor da slider como o atual
         const newComissionSliderValue = parseFloat(e.target.value);
         setSliderValues((oldValues) => {
             return { ...oldValues, comission: newComissionSliderValue };
@@ -47,21 +57,49 @@ const DashbrokersCard = () => {
 
         // Calcular a proporção em relação a comissão e ajustar a proposta
         const proportion =
-            (newComissionSliderValue - comissionValue.min) /
-            (comissionValue.max - comissionValue.min);
+            (newComissionSliderValue - (oficio.properties["(R$) Comissão Mínima - Celer"].number || 0)) /
+            ((oficio.properties["(R$) Comissão Máxima - Celer"].number || 0) - (oficio.properties["(R$) Comissão Mínima - Celer"].number || 0));
+
+        //  define o novo valor da proposta em relação a proporção
         const newProposalSliderValue =
-            proposalValue.max - proportion * (proposalValue.max - proposalValue.min);
+            (oficio.properties["(R$) Proposta Máxima - Celer"].formula?.number || 0) - proportion * ((oficio.properties["(R$) Proposta Máxima - Celer"].formula?.number || 0) - (oficio.properties["(R$) Proposta Mínima - Celer"].formula?.number || 0));
+
         setSliderValues((oldValues) => {
             return { ...oldValues, proposal: newProposalSliderValue };
         });
     };
 
+    const saveProposalAndComission = async () => {
+        setSavingProposalAndComission(true);
+        const req = await api.patch(`/api/notion-api/broker/negotiation/${oficio.id}/`,
+            {
+                proposal: sliderValues.proposal,
+                commission: sliderValues.comission
+            }
+        )
+
+        if (req.status === 202) {
+            toast.success('Proposta e comissão salvas com sucesso!', {
+                icon: <BiCheck className="text-lg fill-green-400" />
+            });
+        }
+
+        if (req.status === 400) {
+            toast.error('Erro ao salvar proposta e comissão!', {
+                icon: <BiX className="text-lg fill-red-500" />
+            });
+        }
+        setSavingProposalAndComission(false);
+    };
+
     useEffect(() => {
         setSliderValues({
-            proposal: proposalValue.min,
-            comission: comissionValue.max,
+            proposal: oficio.properties["Proposta Escolhida - Celer"].number || oficio.properties["(R$) Proposta Mínima - Celer"].formula?.number || 0,
+            comission: oficio.properties["(R$) Comissão Máxima - Celer"].number || 0,
         });
-    }, [proposalValue, comissionValue]);
+    }, [oficio]);
+
+    console.log(oficio);
 
     return (
         <div className="col-span-1 grid gap-5 bg-white dark:bg-boxdark p-5 rounded-md border border-stroke dark:border-strokedark">
@@ -71,31 +109,31 @@ const DashbrokersCard = () => {
                     <div>
                         <p className='text-black dark:text-snow uppercase font-medium'>Nome do Credor:</p>
                         <p
-                            title='Título'
-                            className='max-w-[215px] text-ellipsis overflow-hidden whitespace-nowrap'
+                            title={oficio.properties["Credor"].title[0]?.text.content || "Não informado"}
+                            className='max-w-[277px] text-ellipsis overflow-hidden whitespace-nowrap'
                         >
-                            Luiz Inado da Silva Maciel
+                            {oficio.properties["Credor"].title[0]?.text.content || "Não informado"}
                         </p>
                     </div>
 
                     <div>
                         <p className='text-black dark:text-snow uppercase font-medium'>CPF/CNPJ:</p>
-                        <p>113.633.374-67</p>
+                        <p>{oficio.properties["CPF/CNPJ"].rich_text![0].text.content || "Não informado"}</p>
                     </div>
 
                     <div>
                         <p className='text-black dark:text-snow uppercase font-medium'>TRIBUNAL</p>
                         <p
-                            title='Título'
+                            title={oficio.properties["Tribunal"].select?.name || "Não informado"}
                             className='max-w-[215px] text-ellipsis overflow-hidden whitespace-nowrap'
                         >
-                            Tribunal Regional Federal 1
+                            {oficio.properties["Tribunal"].select?.name || "Não informado"}
                         </p>
                     </div>
 
                     <div>
                         <p className='text-black dark:text-snow uppercase font-medium'>esfera:</p>
-                        <p>FEDERAL</p>
+                        <p>{oficio.properties["Esfera"].select?.name || "Não informado"}</p>
                     </div>
 
                     <button className='flex items-center justify-center gap-2 my-1 py-1 px-4 bg-slate-100 hover:bg-slate-200 dark:bg-boxdark-2/50 dark:hover:bg-boxdark-2/70 rounded-md transition-colors duration-300 text-sm'>
@@ -122,9 +160,16 @@ const DashbrokersCard = () => {
                 {/* ----> end divider <---- */}
 
                 <div className="col-span-6 flex flex-col gap-5">
-                    <div className='flex gap-1 items-center justify-center w-fit ml-auto'>
-                        <CustomCheckbox />
-                        <span className='text-sm font-medium'>Proposta Aceita</span>
+                    <div className='flex justify-between'>
+                        <Button
+                            onClick={saveProposalAndComission}
+                            className='py-1 px-2 h-fit text-sm font-medium'>
+                            {savingProposalAndComission ? "Salvando..." : "Salvar Proposta/Comissão"}
+                        </Button>
+                        <div className='flex gap-1 items-center justify-center w-fit ml-auto'>
+                            <CustomCheckbox />
+                            <span className='text-sm font-medium'>Proposta Aceita</span>
+                        </div>
                     </div>
                     <div className='flex flex-col gap-5 max-h-fit'>
                         <div className="flex items-center justify-between gap-5 2xsm:flex-col md:flex-row">
@@ -139,8 +184,8 @@ const DashbrokersCard = () => {
                                 <input
                                     type="range"
                                     step="0.01"
-                                    min={proposalValue.min}
-                                    max={proposalValue.max}
+                                    min={oficio.properties["(R$) Proposta Mínima - Celer"].formula?.number || 0}
+                                    max={oficio.properties["(R$) Proposta Máxima - Celer"].formula?.number || 0}
                                     value={sliderValues.proposal}
                                     onChange={handleProposalSliderChange}
                                     className="w-full"
@@ -164,8 +209,8 @@ const DashbrokersCard = () => {
                                 <input
                                     type="range"
                                     step="0.01"
-                                    min={comissionValue.min}
-                                    max={comissionValue.max}
+                                    min={oficio.properties["(R$) Comissão Mínima - Celer"].number || 0}
+                                    max={oficio.properties["(R$) Comissão Máxima - Celer"].number || 0}
                                     value={sliderValues.comission}
                                     onChange={handleComissionSliderChange}
                                     className="w-full"
@@ -181,11 +226,16 @@ const DashbrokersCard = () => {
                     {/* ----> observations field <----- */}
                     <div className='w-full h-full'>
                         <p className='mb-2'>Observações:</p>
-                        <textarea
-                            className='w-full rounded-md placeholder:text-sm border-stroke dark:border-strokedark dark:bg-boxdark-2/50 resize-none'
-                            rows={4}
-                            placeholder='Insira uma observação'
-                        />
+                        <div className='relative'>
+                            <textarea
+                                className='w-full rounded-md placeholder:text-sm border-stroke dark:border-strokedark dark:bg-boxdark-2/50 resize-none'
+                                rows={4}
+                                placeholder='Insira uma observação'
+                            />
+                            <Button className='absolute z-2 bottom-3 right-2 py-1 text-sm px-2'>
+                                <BiSave className="text-lg" />
+                            </Button>
+                        </div>
                     </div>
                     {/* <div className='flex items-center justify-center'>
                         <button
