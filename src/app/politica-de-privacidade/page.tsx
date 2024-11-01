77f451a2c172ab487cead040dae7eec601d5d9ae
RobsonFe <robson_20ferreira@hotmail.com>
2024-11-01 15:52:55 -0300

    update:politica

--- src/app/politica-de-privacidade/page.tsx
+++ src/app/politica-de-privacidade/page.tsx
@@ -3,18 +3,11 @@ import { MainFooter } from "@/components/Footer";
 import UnloggedHeader from "@/components/Header/UnloggedHeader";
 import { Politicas } from "@/components/Politicas";
 import { Footer } from "flowbite-react";
-import { Metadata } from "next";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import { Fade } from "react-awesome-reveal";
 import { BiChevronUp } from "react-icons/bi";
 import { BsChevronCompactDown } from "react-icons/bs";
-
-export const metadata: Metadata = {
-  title: "CelerApp | Política de Privacidade",
-  description:
-    "Política de Privacidade da CelerApp, explicando como coletamos e usamos seus dados.",
-};
 
 export default function Privacy() {
   const [isButtonVisible, setIsButtonVisible] = useState<boolean>(false);
