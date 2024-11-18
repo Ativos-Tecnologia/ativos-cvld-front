a0f5a1ab7535fc2169f208da8ef4f49c094e1ca6
RobsonFe <robson_20ferreira@hotmail.com>
2024-11-18 16:58:49 -0300

    WIP:test-change-password

--- src/test/app/change-password/page.test.tsx
+++ src/test/app/change-password/page.test.tsx
@@ -1,6 +1,7 @@
 import ChangePassword from "@/app/change-password/page";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 jest.mock("next/navigation", () => ({
   useRouter() {
@@ -29,57 +30,87 @@ beforeEach(async () => {
 });
 
 describe("Testes na Redefinição de Senha", () => {
-	it("Teste carregar o título da página", async () => {
-		const titulo = await screen.findByText(/Redefinição de senha/i);
-		expect(titulo).toBeInTheDocument();
-	});
-
-	it("Teste carregar o título do formulário", async () => {
-		const tituloDoFormulario = await screen.findByRole("heading", {
-      name: "Redefinição de senha",
-      level: 2,
-    });
-		expect(tituloDoFormulario).toBeInTheDocument();
-	});
-
-	it("Teste para carregar as labels do formulário", async () => {
-
-		const senha = await screen.findByText("Nova senha");
-		expect(senha).toBeInTheDocument();
-
-		const confirmarSenha = await screen.findByText("Repita a nova senha");
-		expect(confirmarSenha).toBeInTheDocument();
-
-	});
-
-	it("Teste para carregar o botão de redefinir senha", async () => {
-		const botao = await screen.findByRole("button", {
-			name: "Confirmar",
+	describe("Teste do titulo da página", () => {
+		it("Teste carregar o título da página", async () => {
+      const titulo = await screen.findByText(/Redefinição de senha/i);
+      expect(titulo).toBeInTheDocument();
 		});
-		expect(botao).toBeInTheDocument();
-	});
-
-	it("Teste para inserir e redefinir a senha", async () => {
-
-		const inputSenha = screen.getAllByLabelText(/Nova senha/i) as HTMLInputElement[];
-		const inputConfirmarSenha = screen.getAllByLabelText(/Repita a nova senha/i) as HTMLInputElement[];
-		const botao = screen.getByRole("button", {
-			name: "Confirmar",
-		});
-
-		fireEvent.change(inputSenha[0], { target: { value: "testeNovaSenha" } });
-     expect(inputSenha[0].value).toBe("testeNovaSenha");
-		 
-		fireEvent.change(inputConfirmarSenha[0], { target: { value: "testeNovaSenha" } });
-		expect(inputConfirmarSenha[0].value).toBe("testeNovaSenha");
 		
-		expect(inputSenha[0]).toBeInTheDocument();
-		expect(inputConfirmarSenha[0]).toBeInTheDocument();
-		expect(botao).toBeInTheDocument();
+		it("Teste carregar o título do formulário", async () => {
+      const tituloDoFormulario = await screen.findByRole("heading", {
+        name: "Redefinição de senha",
+        level: 2,
+      });
+      expect(tituloDoFormulario).toBeInTheDocument();
+    });
+	})
+	
+	describe("Teste para carregar as labels do formulário", () => { 
+		it("Teste para carregar Nova Senha", async () => {
+      const senha = await screen.findByText("Nova senha");
+      expect(senha).toBeInTheDocument();
+		});
+		
+		it("Teste para carregar Repita Nova Senha", async () => {
+			const confirmarSenha = await screen.findByText("Repita a nova senha");
+      expect(confirmarSenha).toBeInTheDocument();
+		 });
 
-		expect(inputSenha[0].value).toBe("testeNovaSenha");
-		expect(inputConfirmarSenha[0].value).toBe("testeNovaSenha");
+	});
+	
+	describe("Teste do botão do formulário", () => { 
+		it("Teste para carregar o botão de redefinir senha", async () => {
+      const botao = await screen.findByRole("button", {
+        name: "Confirmar",
+      });
+      expect(botao).toBeInTheDocument();
+    });
+	});
 
-	}	);
+	describe("Teste de sucesso do input do formulário", () => { 
+		it("Teste para inserir e redefinir a senha", async () => {
+      const inputSenha = screen.getAllByLabelText(
+        /Nova senha/i,
+      ) as HTMLInputElement[];
+      const inputConfirmarSenha = screen.getAllByLabelText(
+        /Repita a nova senha/i,
+      ) as HTMLInputElement[];
+      const botao = screen.getByRole("button", {
+        name: "Confirmar",
+      });
+
+      fireEvent.change(inputSenha[0], { target: { value: "testeNovaSenha" } });
+      expect(inputSenha[0].value).toBe("testeNovaSenha");
+
+      fireEvent.change(inputConfirmarSenha[0], {
+        target: { value: "testeNovaSenha" },
+      });
+      expect(inputConfirmarSenha[0].value).toBe("testeNovaSenha");
+
+      expect(inputSenha[0]).toBeInTheDocument();
+      expect(inputConfirmarSenha[0]).toBeInTheDocument();
+      expect(botao).toBeInTheDocument();
+
+      expect(inputSenha[0].value).toBe("testeNovaSenha");
+      expect(inputConfirmarSenha[0].value).toBe("testeNovaSenha");
+    });
+	});
+
+	describe("Teste de erro do input do formulário", () => { 
+		it("Teste para inserir e redefinir a senha", async () => {
+      const inputSenha = screen.getAllByLabelText(
+        /Nova senha/i,
+      ) as HTMLInputElement[];
+  
+      const botao = screen.getByRole("button", {
+        name: "Confirmar",
+			});
+			expect(botao).toBeInTheDocument();
+
+      fireEvent.change(inputSenha[0], { target: { value: 123 } });
+			expect(inputSenha[0].value).toBe("123");
+      await userEvent.click(botao);
+    });
+	});
 
  });
