1a2d179784bb9a2c2f80d86c64a03f43e2e16a98
Luiz Inaldo <luiz.inaldo970@gmail.com>
2024-11-22 11:04:24 -0300

    Finalizando implementações

--- src/components/CrmUi/Backdrop.tsx
+++ src/components/CrmUi/Backdrop.tsx
@@ -6,10 +6,10 @@ const placementClasses = {
     right: "items-center justify-end"
 }
 
-const Backdrop = ({ children, placement = "center" }: { children: React.ReactNode, placement?: keyof typeof placementClasses }) => {
+const Backdrop = ({ children, placement = "center", isOpen }: { children: React.ReactNode, placement?: keyof typeof placementClasses, isOpen: boolean }) => {
 
     return (
-        <div className={`absolute bg-black-2/50 flex ${placementClasses[placement]} w-full h-full top-0 left-0 rounded-md border z-40`}>
+        <div className={`${isOpen && "animate-fade"} absolute bg-black-2/50 flex ${placementClasses[placement]} w-full h-full top-0 left-0 rounded-md border z-40 transition-all duration-300`}>
             {children}
         </div>
     )
--- src/components/CrmUi/ui/Modal/Modal.tsx
+++ src/components/CrmUi/ui/Modal/Modal.tsx
@@ -18,7 +18,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, size, children }) => {
     if (!isOpen) return null;
 
     return (
-        <Backdrop>
+        <Backdrop isOpen={isOpen}>
             <div className={`relative h-fit ${sizes[size]} rounded-lg border border-stroke bg-white p-5 dark:border-strokedark dark:bg-boxdark z-40`}>
                 {children}
             </div>
