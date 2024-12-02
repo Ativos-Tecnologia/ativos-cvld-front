0d0ebed354010feaea4e7762b79519551fb9f34c
RobsonFe <robson_20ferreira@hotmail.com>
2024-12-02 16:55:19 -0300

    Atualiza informações de contato no rodapé e ajusta testes de acesso a links

--- src/test/components/footer/page.test.tsx
+++ src/test/components/footer/page.test.tsx
@@ -6,9 +6,9 @@ jest.mock("next/navigation", () => ({
   useRouter() {
     return {
       push: jest.fn(),
-      pathname: "/",
-      route: "/",
-      asPath: "/",
+      pathname: "/automated-proposal",
+      route: "/automated-proposal",
+      asPath: "/automated-proposal",
       query: {},
     };
   },
@@ -16,7 +16,7 @@ jest.mock("next/navigation", () => ({
     return new URLSearchParams();
   },
   usePathname() {
-    return "/";
+    return "/automated-proposal";
   },
 }));
 
@@ -136,6 +136,122 @@ describe("Teste do componente MainFooter", () => {
 			const link = await screen.getByText("Av. Fernando Simões Barbosa, 266, 8º andar, sala 308 - Boa Viagem");
 			expect(link).toBeInTheDocument();
 		});
+
+		it("Deve renderizar o link de Politica de Privacidade", async () => {
+			const link = await screen.getByText("Política de Privacidade");
+			expect(link).toBeInTheDocument();
+		});
+
+		it("Teste de renderização de Termos e Condições", async () => {
+			const link = await screen.getByText("Termos e Condições");
+			expect(link).toBeInTheDocument();
+		});
 	});
 
+	describe("Teste de acesso ao link Sobre", () => { 
+		it("Teste de acesso ao link Ativos", async () => { 
+			const ativos = await screen.getAllByText("Ativos");
+			expect(ativos[0]).toBeInTheDocument();
+			ativos[0].click();
+			expect(ativos[0]).toHaveAttribute("href", "#");
+		});
+
+		it("Teste de acesso ao link Missão", async () => { 
+			const missao = await screen.getAllByText("Missão");
+			expect(missao[0]).toBeInTheDocument();
+			missao[0].click();
+			expect(missao[0]).toHaveAttribute("href", "#");
+		});
+
+		it("Teste de acesso ao link Visão", async () => { 
+			const visao = await screen.getAllByText("Visão");
+			expect(visao[0]).toBeInTheDocument();
+			visao[0].click();
+			expect(visao[0]).toHaveAttribute("href", "#");
+		});
+
+		it("Teste de acesso ao link Contato", async () => { 
+			const contato = await screen.getAllByText("Contato");
+			expect(contato[0]).toBeInTheDocument();
+			contato[0].click();
+			expect(contato[0]).toHaveAttribute("href", "#");
+		});
+
 	});
+
+	describe("Teste de acesso ao link Produtos", () => { 
+
+		it("Teste de acesso ao link CelerApp", async () => { 
+			const cellerApp = screen.getAllByText("CelerApp");
+			expect(cellerApp[0]).toBeInTheDocument();
+			cellerApp[0].click();
+			expect(cellerApp[0]).toHaveAttribute("href", "/");
+		});
+
+		it("Teste de acesso ao link Lead Magnet", async () => { 
+			const leadMagnet = screen.getAllByText("Lead Magnet");
+			expect(leadMagnet[0]).toBeInTheDocument();
+			leadMagnet[0].click();
+			expect(leadMagnet[0]).toHaveAttribute("href", "/automated-proposal");
+		});
+
+		it("Teste de acesso ao link Recalculador TRF1", async () => { 
+			const recalculador = screen.getAllByText("Recalculador do TRF1");
+			expect(recalculador[0]).toBeInTheDocument();
+			recalculador[0].click();
+			expect(recalculador[0]).toHaveAttribute("href", "/retification");
+		});
+
+	});
+
+	describe("Teste de acesso ao link Contato", () => {
+
+	it("Teste de acesso ao link WhatsApp", async () => {
+		const whatsAppNumber = "5581996871762";
+		const message = "Olá! Estou entrando em contato através do CellerApp e preciso tirar uma dúvida.";
+		const whatsappLink = `https://wa.me/${whatsAppNumber}?text=${encodeURIComponent(message)}`;
+		
+		const zapLink = screen.getByRole('link', { name: 'Acesse o WhatsApp' });
+		expect(zapLink).toBeInTheDocument();
+		expect(zapLink).toHaveAttribute('href', whatsappLink);
+	});
+
+		it("Teste de acesso ao link Email", async () => { 
+			const email = screen.getByText("contato@ativosprecatorios.com.br");
+			expect(email).toBeInTheDocument();
+			email.click();
+			expect(email).toHaveAttribute(
+		"href",
+		"mailto:contato@ativosprecatorios.com.br",
+			);
+			
+		});
+
+		it("Teste de acesso ao link Endereço", async () => {
+			const address = screen.getByText("Av. Fernando Simões Barbosa, 266, 8º andar, sala 308 - Boa Viagem");
+			expect(address).toBeInTheDocument();
+			address.click();
+			expect(address).toHaveAttribute(
+				"href",
+				"https://maps.app.goo.gl/PLjD95FpCywaRiFz5",
+			);
+		});
+	});
+
+	describe("Teste de acesso ao link Politica de Legal", () => { 
+		it("Teste de acesso ao link Politica de Privacidade", async () => { 
+			const link = screen.getByText("Política de Privacidade");
+			expect(link).toBeInTheDocument();
+			link.click();
+			expect(link).toHaveAttribute("href", "/politica-de-privacidade");
+		});
+
+		it("Teste de acesso ao link Termos e Condições", async () => { 
+			const link = screen.getByText("Termos e Condições");
+			expect(link).toBeInTheDocument();
+			link.click();
+			expect(link).toHaveAttribute("href", "/termos-e-condicoes");
+		});
+	});
+
+});
