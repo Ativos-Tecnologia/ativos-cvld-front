9241206c5ab27fb0415ee97e9125e8851c8e5139
Jarbas Gouveia <gouvik.dev@gmail.com>
2025-01-04 19:14:11 -0300

    Adds projected rentability calculation
    Introduces function to calculate projected rentability until payment date
    Updates chart to include projected values
    Refines input fields and labels in Juridico details component
    Fixes type and formatting issues in rentability functions

--- src/functions/wallet/rentability.ts
+++ src/functions/wallet/rentability.ts
@@ -6,7 +6,7 @@ import { IWalletResponse } from "@/interfaces/IWallet";
 //   return Number.isNaN(result) ? 0 : result;
 // }
 
-export function handleMesesAteOPagamento(data: IWalletResponse) {
+export function handleMesesAteOPagamento(data: IWalletResponse): number {
 
   const data_aquisicao = new Date(data?.result[data?.result.length - 1].data_atualizacao);
   const previsao_de_pgto = new Date(data?.previsao_de_pgto);
@@ -14,6 +14,8 @@ export function handleMesesAteOPagamento(data: IWalletResponse) {
   const diffMonths =
     Math.abs(previsao_de_pgto.getTime() - data_aquisicao.getTime()) /
     (1000 * 60 * 60 * 24 * 30);
+
+    console.log(diffMonths);
 
   return Number.isNaN(diffMonths) ? 0 : diffMonths;
 }
@@ -26,5 +28,5 @@ export function handleMesesAteOPagamento(data: IWalletResponse) {
 // }
 
 export function handleRentabilidadeAM(rentabilidadeAnual: number) {
-  return Math.pow(1 + rentabilidadeAnual, 1 / 12) - 1;
+  return Number((Math.pow(1 + rentabilidadeAnual, 1 / 12) - 1).toFixed(4));
 }
