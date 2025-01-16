e5b8d0477d5c152553bdd571e507fc413f75d6c2
Jarbas Gouveia <gouvik.dev@gmail.com>
2025-01-16 11:31:44 -0300

    Adds acquisition date to Wallet and LegalDetails components
    Introduces a new field 'data_de_aquisicao' to track the acquisition date in both Wallet and LegalDetails components for better data management.

--- src/components/Juridico/Details.tsx
+++ src/components/Juridico/Details.tsx
@@ -73,6 +73,7 @@ export const LegalDetails = ({ id }: JuridicoDetailsProps) => {
     valor_projetado: 0,
     previsao_de_pgto: "",
     rentabilidade_anual: 0,
+    data_de_aquisicao: "",
     result: [
       {
         data_atualizacao: "",
