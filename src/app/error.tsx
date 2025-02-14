d707116aee0185b4f0f5ec95ff6859714c424ca8
Lucas <lucasaraujodesa@hotmail.com>
2025-02-14 12:29:27 -0300

    ✨  Refactor: muda img para next/image na página de error

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,8 @@
-'use client'
-import React, { useEffect } from 'react'
+'use client';
+import React, { useEffect } from 'react';
 import { AiFillHome } from 'react-icons/ai';
+import illustration from '../../public/images/illustration/illustration-01.svg';
+import Image from 'next/image';
 
 interface ErrorProps {
     error: Error;
@@ -8,10 +10,9 @@ interface ErrorProps {
 }
 
 export default function Error({ error, reset }: ErrorProps) {
-
     useEffect(() => {
-        console.error(error)
-    }, [error])
+        console.error(error);
+    }, [error]);
 
     return (
         // <div className='bg-gray h-screen flex flex-col items-center justify-center'>
@@ -36,49 +37,35 @@ export default function Error({ error, reset }: ErrorProps) {
         //         </button>
         //     </div>
         // </div>
-        <div className='h-screen flex flex-col items-center justify-center bg-snow dark:bg-boxdark'>
-
-            <div className='grid grid-cols-2 gap-5'>
-                <div className='col-span-1'>
-                    <img
-                        src={'/images/illustration/illustration-01.svg'}
-                        alt='meme do the office'
-                        className='w-full'
-                    />
+        <div className="flex h-screen flex-col items-center justify-center bg-snow dark:bg-boxdark">
+            <div className="grid grid-cols-2 gap-5">
+                <div className="col-span-1">
+                    <Image src={illustration} alt="meme do the office" className="w-full" />
                 </div>
-                <div className='col-span-1 flex flex-col items-center pt-10'>
-                    <h1 className='text-3xl text-center mb-5 font-bold'>
-                        OOOPS, ALGO DEU ERRADO!
-                    </h1>
-                    <div className='flex gap-2'>
-                        <span className='text-center italic font-medium'>
-                            tipo:
-                        </span>
-                        <span className='text-center text-black-2 dark:text-white italic font-light'>
+                <div className="col-span-1 flex flex-col items-center pt-10">
+                    <h1 className="mb-5 text-center text-3xl font-bold">OOOPS, ALGO DEU ERRADO!</h1>
+                    <div className="flex gap-2">
+                        <span className="text-center font-medium italic">tipo:</span>
+                        <span className="text-center font-light italic text-black-2 dark:text-white">
                             {error.name}
                         </span>
                     </div>
-                    <div className='flex gap-2'>
-                        <span className='text-center italic font-medium'>
-                            erro:
-                        </span>
-                        <span className='text-center text-black-2 dark:text-white italic font-light'>
+                    <div className="flex gap-2">
+                        <span className="text-center font-medium italic">erro:</span>
+                        <span className="text-center font-light italic text-black-2 dark:text-white">
                             {error.message}
                         </span>
                     </div>
-                    <p className='my-5'>
-                        Contate o administrador para mais informações.
-                    </p>
+                    <p className="my-5">Contate o administrador para mais informações.</p>
                     <button
                         onClick={reset}
-                        className='flex items-center gap-2 px-3 py-2 bg-blue-700 hover:bg-blue-800 transition-colors duration-200 rounded-md text-snow'
+                        className="flex items-center gap-2 rounded-md bg-blue-700 px-3 py-2 text-snow transition-colors duration-200 hover:bg-blue-800"
                     >
                         <AiFillHome />
                         Voltar para home
                     </button>
                 </div>
             </div>
-
         </div>
-    )
+    );
 }
